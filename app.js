7cbb828e8585a3346856b43c14df1a1f992d1f74
Dora Kutsenko <kk.tasha@yandex.ru>
2023-06-28 05:44:25 +0300

    first

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes/routes');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '649b3e5702670e62e27cb219', // ID пользователя из mongo.
+  };
+
+  next();
+});
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .then(() => {
+    console.log('БД подключена');
+  })
+  .catch(() => {
+    console.log('Не удалось подключиться к БД');
+  });
+
+app.get('/', (req, res) => {
+  res.send('Вывод инф-ции на страницу...');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
---
+++ controllers/users.js
@@ -0,0 +1,83 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя.',
+        });
+      } else {
+        res.status(500).send({ message: 'Ошибка по умолчанию' });
+      }
+    });
+};
+
+module.exports.getUserId = (req, res) => {
+  User
+    .findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(404)
+          .send({ message: 'Пользователь c указанным _id не найден' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res
+          .status(400)
+          .send({
+            message: 'Переданы некорректные данные при поиске пользователя',
+          });
+      } else {
+        res.status(500).send({ message: 'Ошибка по умолчанию' });
+      }
+    });
+};
+
+module.exports.updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({
+            message: 'Переданы некорректные данные при обновлении профиля',
+          });
+      }
+      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+    });
+};
+
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({
+            message: 'Переданы некорректные данные при обновлении аватара',
+          });
+      }
+      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+    });
+};
+
